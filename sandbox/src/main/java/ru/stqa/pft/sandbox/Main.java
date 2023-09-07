47b46975c44562000b5d9d678a37456805c55b16
Belokurov Georgy <Belokurov.gm@gmail.com>
2023-09-07 18:37:51 +0300

    homework with dots part 1

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Main.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Main.java
@@ -2,9 +2,14 @@ package ru.stqa.pft.sandbox;
 
 public class Main {
     public static void main(String[] args) {
-        Point point = new Point(-5, 10);
+        Point point = new Point();
+        point.firstDot = 100;
+        point.secondDot = 10;
 
         System.out.println("The coordinate of dots = " + point.firstDot + " and " + point.secondDot);
-        System.out.println("Check method into the class of Point: Distance between two dots = " + point.distance());
+        System.out.println("Check method into the class of Point: Distance between two dots = " + distance(point, point));
+    }
+    public static double distance(Point a, Point b){
+        return a.secondDot - b.firstDot;
     }
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -3,17 +3,4 @@ package ru.stqa.pft.sandbox;
 public class Point {
     public double firstDot;
     public double secondDot;
-
-    public Point(){
-        firstDot = 0;
-        secondDot = 0;
-    }
-    public Point(double firstDot, double secondDot){
-        this.firstDot = firstDot;
-        this.secondDot = secondDot;
-    }
-
-    public double distance(){
-        return this.secondDot - this.firstDot;
-    }
 }
